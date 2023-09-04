635f03d476cd05a4e1808d759c324f7ad86e9125
wonjunJang <nuriyooyoung@gmail.com>
2023-09-04 10:01:18 +0900

    update[ver1.2]

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 from modules.ai import WonJunAI
+import env
 app = Flask(__name__)
-ai = WonJunAI()
+ai = WonJunAI(env.PT_ROUTE)
 
 contents = [
-    "이곳에서 챗봇을 체험해보세요!",
-    "누구나 무료로 사용할 수 있습니다!",
-    "어머나~ 이건 꼭 해야 해!"
+    "안녕하세요! AbaChat 입니다",
+    "새학년, 새학기 모르는 사람들에게 자기소개를 하는것이 귀찮거나, 부끄럽지 않으신가요? 그럴때는 AbaChat을 사용해보세요! AbaChat은 부끄러운 당신을 위한 아바타 자기소개 챗봇입니다. 자신을 소개하는 자리에서 자신 아바타 챗봇 링크를 주고 자신을 소개해 보세요!"
 ]
 
 @app.route('/')
 def main():
-    return render_template('index.html', title="IntroM", contents=contents)
+    return render_template('index.html', title="AbaChat", contents=contents)
 
 @app.route('/chatbot', methods=['GET', 'POST'])
 def chatbot():
@@ -22,9 +22,9 @@ def chatbot():
             return {'text': value}
     except Exception as e:
         print(e)
-        return render_template('chatbot.html', title='IntroM')
+        return render_template('chatbot.html', title='AbaChat')
 
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8000)
+    app.run('0.0.0.0', debug = True, port=8000)
--- modules/ai.py
+++ modules/ai.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import PreTrainedTokenizerFast
-
+import env
 Q_TKN = "<usr>"
 A_TKN = "<sys>"
 BOS = '</s>'
@@ -10,27 +10,47 @@ SENT = '<unused1>'
 PAD = '<pad>'
 
 koGPT2_TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
-            bos_token=BOS, eos_token=EOS, unk_token='<unk>',
-            pad_token=PAD, mask_token=MASK) 
-model = torch.load('./static/test.pt')
+                                                           bos_token=BOS, eos_token=EOS, unk_token='<unk>',
+                                                           pad_token=PAD, mask_token=MASK)
 
-print(model)
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+elif torch.backends.mps.is_available():
+    device = 'mps'
+
+device = torch.device(device)
+
+print(f"{device} detected")
+
 print("loading ai components successfully")
 
 
 class WonJunAI:
-    def __init__(self, p1="사람", p2="AI"):
+    def __init__(self, path, p1="사람", p2="AI"):
+        self.model = torch.load(path, map_location=device)
         self.p1 = p1
         self.p2 = p2
+        
+    def __repr__(self):
+        return self.model.__repr__()
+
     @torch.no_grad()
     def create_response(self, text):
         gen_text = ""
         while True:
-            input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + text + SENT + A_TKN + gen_text)).unsqueeze(dim=0)
-            pred = model(input_ids)
+            input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(
+                Q_TKN + text + SENT + A_TKN + gen_text)).unsqueeze(dim=0)
+            pred = self.model(input_ids)
             pred = pred.logits
-            gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
+            gen = koGPT2_TOKENIZER.convert_ids_to_tokens(
+                torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
             if gen == EOS:
                 break
             gen_text += gen.replace("▁", " ")
         return gen_text
+
+
+if __name__ == "__main__":
+    model = WonJunAI(env.PT_ROUTE)
+    print(model)
